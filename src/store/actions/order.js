744bf847a7d0836e886161c5c6ad82ecc067ec82
Kholoud22 <khlood_mohamed@hotmail.com>
2019-08-24 19:19:43 +0200

    Update order.js

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -67,7 +67,6 @@ export const fetchOrders = (token, userId) => {
         const queryParams = '?auth=' + token ;
         axios.get( '/orders.json' + queryParams)
             .then( res => {
-                console.log(res);
                 const fetchedOrders = [];
                 for ( let key in res.data ) {
                     fetchedOrders.push( {
